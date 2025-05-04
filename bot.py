084b29663c16fbb04c786a9f0d2eff20cedd5d27
stalker7521 <yaristalker@gmail.com>
2025-05-04 20:08:27 +0500

    commit old version

--- bot.py
+++ bot.py
@@ -1,15 +1,10 @@
-import json
-from tempfile import TemporaryDirectory
-from pathlib import Path
-import streamlit as st
 import telebot
-from telebot.types import ReactionTypeEmoji
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from settings import TOKEN, SPREADSHEET_ID, WORKSHEET_NAME, user_column_map, SCOPE
 from datetime import datetime
 
-# CREDS_FILE = './creds/so_test.json'  # Путь к файлу credential.json Больше не нужен
+CREDS_FILE = './creds/so_test.json'  # Путь к файлу credential.json
 
 # Инициализация бота
 bot = telebot.TeleBot(TOKEN)
@@ -20,16 +15,8 @@ bot = telebot.TeleBot(TOKEN)
 # /todo добавить логгирование бота
 # Функция для подключения к Google Sheets
 def get_gsheet_client():
-    cred_str = st.secrets['CREDS']
-    creds_obj = json.loads(cred_str)
-    tmp_dir = TemporaryDirectory()
-    tmp_dir_path = Path(tmp_dir.name)
-    json_path = tmp_dir_path / 'creds.json'
-    with open(json_path, 'w') as f:
-        f.write(json.dumps(creds_obj, indent=2))
-    creds = ServiceAccountCredentials.from_json_keyfile_name(json_path, SCOPE)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPE)
     client = gspread.authorize(creds)
-    tmp_dir.cleanup()
     return client
 
 
@@ -42,26 +29,22 @@ def write_to_sheet(value, usr_id, date):
     try:
         client = get_gsheet_client()
         sheet = client.open_by_key(SPREADSHEET_ID).worksheet(WORKSHEET_NAME)
+
         """Ищем строку с указанной датой"""
         dates = sheet.col_values(1)  # Получаем все даты из столбца A (он с датами)
+
         if user_column_map[usr_id]:
-            usr_name = user_column_map[usr_id]  # вытаскиваем из словаря Имя пользователя по его tg-id
-            col_names = sheet.row_values(1) # список всех имен пользователей
-            col_index = col_names.index(usr_name) + 1
+            column = user_column_map[usr_id]  # вытаскиваем из словаря столбец пользователя по его tg-id
             row_num = dates.index(date) + 1  # +1 т.к. нумерация с 1
-            sheet.update_cell(row_num, col_index, value)  # добавляем в последнюю ячейку определенного столбца данные
+            sheet.update_cell(row_num, column, value)  # добавляем в последнюю ячейку определенного столбца данные
             print(f'Value "{value}" appended to sheet')
 
     except Exception as e:
         print(f'An error occurred: {e}')
 
 
-def plus_message_handlig(message):
-    return message.text.startswith('+') and message.text[1:].isdigit()
-
-
 # Обработчик сообщений, начинающихся с "+" и числа
-@bot.message_handler(func=plus_message_handlig)
+@bot.message_handler(func=lambda message: message.text.startswith('+') and message.text[1:].isdigit())
 def handle_number_message(message):
     number = message.text[1:]
     user_id = str(message.from_user.id)
@@ -70,11 +53,8 @@ def handle_number_message(message):
     write_to_sheet(number, user_id, date)  # записываем число в таблицу
     # list_of_data.append(number)  # добавляем число в список
     # count_of_dist += int(number)  # увеличиваем общий счетчик
-    # bot.reply_to(message, f'Number {number} has been recorded.')
-    bot.set_message_reaction(chat_id=message.chat.id,
-                             message_id=message.id,
-                             reaction=[ReactionTypeEmoji("✍")]
-                             )
+    bot.reply_to(message, f'Number {number} has been recorded.')
+
 
 # Обработчик сообщений вида: +метры дата_куда_нужно_записать_метры
 @bot.message_handler(
@@ -82,17 +62,14 @@ def handle_number_message(message):
         message.text.split()) == 2)
 def handle_number_with_data_message(message):
     number = message.text.split()[0][1:]
-    date = str(message.text.split()[1])  # ToDo проверять формат даты
+    date = str(message.text.split()[1])
     user_id = str(message.from_user.id)
     print(f'ID пользователя, который ввел данные: {user_id}')
     write_to_sheet(number, user_id, date)  # записываем число в таблицу
     # list_of_data.append(number)  # добавляем число в список
     # count_of_dist += int(number)  # увеличиваем общий счетчик
     bot.reply_to(message, f'Number {number} has been recorded on the date {date}')
-    bot.set_message_reaction(chat_id=message.chat.id,
-                             message_id=message.id,
-                             reaction=[ReactionTypeEmoji("✍")]
-                             )
+
 
 # @bot.message_handler(commands=['list'])
 # def print_list_of_data(message):
@@ -112,6 +89,5 @@ def handle_start(message):
 
 # Запуск бота
 if __name__ == '__main__':
-    st.write('Bot is running...')
+    print('Bot is running...')
     bot.polling(none_stop=True)
-    st.stop()
--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
-import json
-import streamlit as st
+import os
+import ast
+from dotenv import load_dotenv
 
-TOKEN = st.secrets['TOKEN']
-SPREADSHEET_ID = st.secrets['SPREADSHEET_ID']
-user_column_map = json.loads(st.secrets['user_column_map'])
-WORKSHEET_NAME = 'test'
-SCOPE = ['https://www.googleapis.com/auth/spreadsheets']
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
+WORKSHEET_NAME = os.getenv('WORKSHEET_NAME')
+user_column_map = ast.literal_eval(os.getenv('USER_COLUMN_MAP'))  # magic преобразование строки в словарь
+SCOPE = ast.literal_eval(os.getenv('SCOPE'))  # Google Sheets API credentials
