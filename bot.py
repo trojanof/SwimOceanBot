6131244641b960a0ee465a82e1a6fe69381ab3d0
stalker7521 <yaristalker@gmail.com>
2025-05-18 23:25:23 +0500

    UPDATE: changed user_id to user_name

--- bot.py
+++ bot.py
@@ -10,8 +10,6 @@ CREDS_FILE = './creds/so_test.json'  # Путь к файлу credential.json
 bot = telebot.TeleBot(TOKEN)
 
 
-# /todo захостить бота и сделать его автономным
-
 # /todo добавить логгирование бота
 # Функция для подключения к Google Sheets
 def get_gsheet_client():
@@ -21,7 +19,7 @@ def get_gsheet_client():
 
 
 # Функция для записи данных в Google Sheets
-def write_to_sheet(value, usr_id, date):
+def write_to_sheet(value, usr_name, date):
     """Берем текущую дату"""
     if date == "":
         date = datetime.now().strftime("%d.%m.%Y")  # -> "13.04.2025"
@@ -33,8 +31,8 @@ def write_to_sheet(value, usr_id, date):
         """Ищем строку с указанной датой"""
         dates = sheet.col_values(1)  # Получаем все даты из столбца A (он с датами)
 
-        if user_column_map[usr_id]:
-            column = user_column_map[usr_id]  # вытаскиваем из словаря столбец пользователя по его tg-id
+        if user_column_map[usr_name]:
+            column = user_column_map[usr_name]  # вытаскиваем из словаря столбец пользователя по его tg-name
             row_num = dates.index(date) + 1  # +1 т.к. нумерация с 1
             sheet.update_cell(row_num, column, value)  # добавляем в последнюю ячейку определенного столбца данные
             print(f'Value "{value}" appended to sheet')
@@ -47,7 +45,7 @@ def write_to_sheet(value, usr_id, date):
 @bot.message_handler(func=lambda message: message.text.startswith('+') and message.text[1:].isdigit())
 def handle_number_message(message):
     number = message.text[1:]
-    user_id = str(message.from_user.id)
+    user_name = str(message.from_user.username)
     """
     Извлекаем дату сообщения. Дата в формате unix timestamp. Прибавляем 18000 = 5 часов т.к. дата хранится в GMT+0
     """
@@ -55,8 +53,8 @@ def handle_number_message(message):
 
     date = date_obj.strftime("%d.%m.%Y")  # преобразуем в нужный формат даты
     print(date)
-    print(f'ID пользователя, который ввел данные: {user_id}')
-    write_to_sheet(number, user_id, date)  # записываем число в таблицу
+    print(f'ID пользователя, который ввел данные: {user_name}')
+    write_to_sheet(number, user_name, date)  # записываем число в таблицу
     # list_of_data.append(number)  # добавляем число в список
     # count_of_dist += int(number)  # увеличиваем общий счетчик
     bot.reply_to(message, f'Number {number} has been recorded.')
@@ -69,9 +67,9 @@ def handle_number_message(message):
 def handle_number_with_data_message(message):
     number = message.text.split()[0][1:]
     date = str(message.text.split()[1])
-    user_id = str(message.from_user.id)
-    print(f'ID пользователя, который ввел данные: {user_id}')
-    write_to_sheet(number, user_id, date)  # записываем число в таблицу
+    user_name = str(message.from_user.id)
+    print(f'ID пользователя, который ввел данные: {user_name}')
+    write_to_sheet(number, user_name, date)  # записываем число в таблицу
     # list_of_data.append(number)  # добавляем число в список
     # count_of_dist += int(number)  # увеличиваем общий счетчик
     bot.reply_to(message, f'Number {number} has been recorded on the date {date}')
